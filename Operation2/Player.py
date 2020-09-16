9b87bb1512a759cdbad4ed16f7733ca8b8083964
Harri <harri.karhinen1@gmail.com>
2020-09-16 20:49:29 +0300

    jos saisi vain toimimaan

---
+++ Operation2/Player.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+import urllib.parse
+import urllib.request
+import re
+import random as rd
+import numpy as np
+import os
+import youtube_dl
+# ##################################################
+print('omega')
